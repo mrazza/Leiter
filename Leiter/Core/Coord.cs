5bb4ba3f9986adaca569580534e53af525ad3943
Matt Razza <504088+mrazza@users.noreply.github.com>
2025-12-23 17:56:54 -0500

    Include missing Coord operator change.

--- Leiter/Core/Coord.cs
+++ Leiter/Core/Coord.cs
@@ -1,3 +1,6 @@
 namespace Leiter.Core;
 
-public readonly record struct Coord(int X, int Y);
+public readonly record struct Coord(int X, int Y)
+{
+    public static Coord operator +(Coord a, Coord b) => new(a.X + b.X, a.Y + b.Y);
+}
